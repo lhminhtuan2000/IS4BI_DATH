5f02d836352b510631cf4d3801c53bd4b417dd79
minhtri06 <pmtri.tvv@outlook.com>
2022-12-27 17:29:56 +0700

    Add source_id in all tables

--- sql_script/nds.sql
+++ sql_script/nds.sql
@@ -14,6 +14,8 @@ CREATE TABLE [phu_city]
     [name] NVARCHAR(255),
     phu_group_id INT,
 
+    source_id INT,
+
     CONSTRAINT pk__phu_city PRIMARY KEY (id)
 )
 GO
@@ -23,6 +25,8 @@ CREATE TABLE [phu_group]
 (
     id INT IDENTITY,
     phu_group_name NVARCHAR(255),
+
+    source_id INT,
 
     CONSTRAINT pk__phu_group PRIMARY KEY (id)
 )
@@ -34,6 +38,8 @@ CREATE TABLE [outbreak_group]
     id INT IDENTITY,
     outbreak_group NVARCHAR(255),
 
+    source_id INT,
+
     CONSTRAINT pk__outbreak_group PRIMARY KEY (id)
 )
 GO
@@ -42,9 +48,13 @@ GO
 CREATE TABLE [ongoing_outbreaks_phu]
 (
     id INT IDENTITY,
+    
     [date] DATE,
     phu_id INT,
-    outbreak_group_id INT
+    outbreak_group_id INT,
+    source_id INT,
+    
+    number_ongoing_outbreaks INT,
 
     CONSTRAINT pk__ongoing_outbreaks_phu PRIMARY KEY (id)
 )
@@ -54,9 +64,12 @@ GO
 CREATE TABLE [vaccines_by_age_phu] 
 (
     id INT IDENTITY,
+    
     [date] DATE,
     phu_id INT,
     age_group_id INT,
+    source_id INT,
+
     at_least_one_dose_cumulative INT,
     second_dose_cumulative INT,
     fully_vaccinated_cumulative INT,
@@ -72,6 +85,8 @@ CREATE TABLE [outcome]
     id INT IDENTITY,
     outcome NVARCHAR(255),
 
+    source_id INT,
+
     CONSTRAINT pk__outcome PRIMARY KEY (id)
 )
 GO
@@ -81,6 +96,8 @@ CREATE TABLE [exposure]
 (
     id INT IDENTITY,
     exposure NVARCHAR(255),
+
+    source_id INT,
 
     CONSTRAINT pk__exposure PRIMARY KEY (id)
 )
@@ -92,6 +109,8 @@ CREATE TABLE [age_group]
     id INT IDENTITY,
     age_group NVARCHAR(255),
 
+    source_id INT,
+
     CONSTRAINT pk__age_group PRIMARY KEY (id)
 )
 GO
@@ -100,6 +119,10 @@ GO
 CREATE TABLE [case] 
 (
     id INT IDENTITY,
+    
+    case_nk INT,
+    source_id INT,
+    
     reported_date DATE,
     reporting_phu_id INT,
     gender NVARCHAR(255),
@@ -115,7 +138,10 @@ GO
 CREATE TABLE [public_health_unit]
 (
     id INT IDENTITY,
+    
     phu_nk INT UNIQUE,
+    source_id INT,
+    
     [name] NVARCHAR(255),
     [address] NVARCHAR(255),
     phu_group_id INT,
@@ -125,6 +151,16 @@ CREATE TABLE [public_health_unit]
     longitude FLOAT,
 
     CONSTRAINT pk__public_health_unit PRIMARY KEY (id)
+)
+GO
+
+
+CREATE TABLE [source]
+(
+    id INT IDENTITY,
+    name NVARCHAR(255),
+
+    CONSTRAINT pk__source PRIMARY KEY (id)
 )
 GO
 
@@ -141,12 +177,18 @@ ALTER TABLE [case]
 ALTER TABLE [case]
     ADD CONSTRAINT fk__case__public_health_unit
     FOREIGN KEY (reporting_phu_id) REFERENCES public_health_unit (id)
+ALTER TABLE [case]
+    ADD CONSTRAINT fk__case__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
 GO
 
 
 ALTER TABLE [phu_city]
     ADD CONSTRAINT fk__phu_city__phu_group
     FOREIGN KEY (phu_group_id) REFERENCES phu_group (id)
+ALTER TABLE [phu_city]
+    ADD CONSTRAINT fk__phu_city_phu__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
 GO
 
 
@@ -156,12 +198,18 @@ ALTER TABLE [ongoing_outbreaks_phu]
 ALTER TABLE [ongoing_outbreaks_phu]
     ADD CONSTRAINT fk__ongoing_outbreaks_phu__public_health_unit
     FOREIGN KEY (phu_id) REFERENCES public_health_unit (id)
+ALTER TABLE [ongoing_outbreaks_phu]
+    ADD CONSTRAINT fk__ongoing_outbreaks_phu__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
 GO
 
 
 ALTER TABLE [public_health_unit]
     ADD CONSTRAINT fk__public_health_unit__phu_group
     FOREIGN KEY (phu_group_id) REFERENCES phu_group (id)
+ALTER TABLE [public_health_unit]
+    ADD CONSTRAINT fk__public_health_unit__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
 GO
 
 
@@ -171,4 +219,37 @@ ALTER TABLE [vaccines_by_age_phu]
 ALTER TABLE [vaccines_by_age_phu]
     ADD CONSTRAINT fk__public_health_unit__age_group
     FOREIGN KEY (age_group_id) REFERENCES age_group (id)
+ALTER TABLE [vaccines_by_age_phu]
+    ADD CONSTRAINT fk__vaccines_by_age_phu__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
+GO
+
+
+ALTER TABLE [phu_group]
+    ADD CONSTRAINT fk__phu_group__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
+GO
+
+
+ALTER TABLE [outbreak_group]
+    ADD CONSTRAINT fk__outbreak_group__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
+GO
+
+
+ALTER TABLE [outcome]
+    ADD CONSTRAINT fk__outcome__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
+GO
+
+
+ALTER TABLE [exposure]
+    ADD CONSTRAINT fk__exposure__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
+GO
+
+
+ALTER TABLE [age_group]
+    ADD CONSTRAINT fk__age_group__source
+    FOREIGN KEY (source_id) REFERENCES source (id)
 GO
